755621e99a82c493eef4598e9f43137ce4f15532
zivillian <devel@zivillian.de>
2016-01-20 19:44:26 +0100

    initial commit (untested)

---
+++ mono/Led.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace rpi_ws281x
+{
+    [StructLayout(LayoutKind.Explicit)]
+    public struct Led
+    {
+        [FieldOffset(0)]
+        public byte A;
+
+        [FieldOffset(1)]
+        public byte R;
+
+        [FieldOffset(2)]
+        public byte G;
+
+        [FieldOffset(3)]
+        public byte B;
+
+        [FieldOffset(0)]
+        public int Color;
+        
+    }
+}
