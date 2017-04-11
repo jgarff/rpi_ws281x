4c0b23e1985bf6b609313915ea07ea83e3efc752
Phil Howard <phil@gadgetoid.com>
2017-04-11 10:59:50 +0000

    Reverted to old detection, added 0x9200c1

--- rpihw.c
+++ rpihw.c
@@ -218,6 +218,14 @@ static const rpi_hw_t rpi_hw_info[] = {
         .videocore_base = VIDEOCORE_BASE_RPI,
         .desc = "Pi Zero W v1.1",
     },
+    {
+        .hwver  = 0x9200c1,
+        .type = RPI_HWVER_TYPE_PI1,
+        .periph_base = PERIPH_BASE_RPI,
+        .videocore_base = VIDEOCORE_BASE_RPI,
+        .desc = "Pi Zero W v1.1",
+    },
+
 
     //
     // Model A+
@@ -357,8 +365,11 @@ const rpi_hw_t *rpi_dt_hw_detect(void){
         return &board_info;
 
     }
-
-    return NULL;
+    else
+    {
+        // Fail over to classic detection
+        return rpi_hw_detect();
+    }
 }
 
 const rpi_hw_t *rpi_hw_detect(void)
