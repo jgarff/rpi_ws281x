4d1c36383dfded2df9d465bacefff24fc93ff68f
penfold42 <penfold42@users.noreply.github.com>
2017-05-21 01:14:34 +1000

    Re added "Pi Zero W v1.1", .hwver = 0x9000c1, (#181)

--- rpihw.c
+++ rpihw.c
@@ -217,6 +217,13 @@ static const rpi_hw_t rpi_hw_info[] = {
         .videocore_base = VIDEOCORE_BASE_RPI,
         .desc = "Pi Zero W",
     },
+    {
+        .hwver  = 0x9000c1,
+        .type = RPI_HWVER_TYPE_PI1,
+        .periph_base = PERIPH_BASE_RPI,
+        .videocore_base = VIDEOCORE_BASE_RPI,
+        .desc = "Pi Zero W v1.1",
+    },
 
     //
     // Model A+
