5b8dbbada2c050543d73446e5cc5e0414bbffc9e
Pandora <pandora@techfo.xyz>
2018-01-03 02:58:49 +0000

    Fix for non-root users can use SPI

--- mailbox.h
+++ mailbox.h
@@ -38,7 +38,7 @@ unsigned mem_alloc(int file_desc, unsigned size, unsigned align, unsigned flags)
 unsigned mem_free(int file_desc, unsigned handle);
 unsigned mem_lock(int file_desc, unsigned handle);
 unsigned mem_unlock(int file_desc, unsigned handle);
-void *mapmem(unsigned base, unsigned size);
+void *mapmem(unsigned base, unsigned size, const char *mem_dev);
 void *unmapmem(void *addr, unsigned size);
 
 unsigned execute_code(int file_desc, unsigned code, unsigned r0, unsigned r1, unsigned r2, unsigned r3, unsigned r4, unsigned r5);
