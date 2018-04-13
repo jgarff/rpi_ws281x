513585e1b7199e81871282c80f475508871309dc
Felix Uhl <iFreilicht@users.noreply.github.com>
2018-04-13 17:40:30 +0200

    Fix Crash in rainbowCycle
    When running this script, I was getting a TypeError: unsupported operand type(s) for &: 'float' and 'int'
    on line 64. `//` returns int instead of float like `/` does, which fixes this issue.

--- python/examples/SK6812_strandtest.py
+++ python/examples/SK6812_strandtest.py
@@ -62,7 +62,7 @@ def rainbowCycle(strip, wait_ms=20, iterations=5):
 	"""Draw rainbow that uniformly distributes itself across all pixels."""
 	for j in range(256*iterations):
 		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel(((i * 256 / strip.numPixels()) + j) & 255))
+			strip.setPixelColor(i, wheel(((i * 256 // strip.numPixels()) + j) & 255))
 		strip.show()
 		time.sleep(wait_ms/1000.0)
 
