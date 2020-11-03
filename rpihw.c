ceeb9b197b9c62d8891e02d3ba27e7c557773578
Phil Howard <phil@gadgetoid.com>
2020-11-03 17:26:44 +0000

    Support for 0xb03114 (Pi 4, 2GB v1.4)

--- rpihw.c
+++ rpihw.c
@@ -114,6 +114,13 @@ static const rpi_hw_t rpi_hw_info[] = {
         .videocore_base = VIDEOCORE_BASE_RPI2,
         .desc = "Pi 4 Model B - 8GB v1.2"
     },
+    {
+        .hwver = 0xb03114,
+        .type = RPI_HWVER_TYPE_PI4,
+        .periph_base = PERIPH_BASE_RPI4,
+        .videocore_base = VIDEOCORE_BASE_RPI2,
+        .desc = "Pi 4 Model B - 2GB v1.4"
+    },
     //
     // Model B Rev 1.0
     //
