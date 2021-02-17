7637eb72f1ef39285fb45c1774cb5cf1217c626d
Kishan Joshi <robokishan.blogspot@gmail.com>
2021-02-17 11:08:03 +0530

    1GB Version rpi hardware resvision added

--- rpihw.c
+++ rpihw.c
@@ -263,6 +263,13 @@ static const rpi_hw_t rpi_hw_info[] = {
         .videocore_base = VIDEOCORE_BASE_RPI2,
         .desc = "Pi Compute Module 4 Rev 1.0"
     },
+	{
+        .hwver = 0xa03140,
+        .type = RPI_HWVER_TYPE_PI4,
+        .periph_base = PERIPH_BASE_RPI4,
+        .videocore_base = VIDEOCORE_BASE_RPI2,
+        .desc = "Pi Compute Module 4 Rev 1.0"
+    },
 
     //
     // Pi Zero
