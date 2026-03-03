f6669b6171e9b30a07a00b6f9984f58400ec542f
Jeremy Garff <jer@jers.net>
2026-03-02 21:31:19 -0700

    Fix kernel compile errors for pi5.

--- rp1_ws281x_pwm/rp1_ws281x_pwm.c
+++ rp1_ws281x_pwm/rp1_ws281x_pwm.c
@@ -471,7 +471,7 @@ int rp1_ws281x_pwm_probe(struct platform_device *pdev) {
     return 0;
 }
 
-int rp1_ws281x_pwm_remove(struct platform_device *pdev) {
+void rp1_ws281x_pwm_remove(struct platform_device *pdev) {
     rp1_ws281x_pwm_cleanup();
 
     misc_deregister(&rp1_ws281x_pwm.mdev);
@@ -481,8 +481,6 @@ int rp1_ws281x_pwm_remove(struct platform_device *pdev) {
     devm_iounmap(&pdev->dev, rp1_ws281x_pwm.regs);
 
     rp1_ws281x_pwm.pdev = NULL;
-
-    return 0;
 }
 
 static const struct of_device_id rp1_ws281x_pwm_of_match[] = {
--- rp1_ws281x_pwm/rp1_ws281x_pwm.h
+++ rp1_ws281x_pwm/rp1_ws281x_pwm.h
@@ -26,5 +26,21 @@ typedef struct {
 } rp1_ws281x_pwm_ioctl_xfer_t;
 
 
+//
+// Kernel module function declarations
+//
+void rp1_ws281x_pwm_chan(int channel, int invert);
+void rp1_ws281x_pwm_init(int channel, int invert);
+void rp1_ws281x_pwm_cleanup(void);
+int rp1_ws281x_pwm_open(struct inode *inode, struct file *file);
+int rp1_ws281x_pwm_release(struct inode *inode, struct file *file);
+long rp1_ws281x_pwm_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
+void rp1_ws281x_dma_callback(void *param);
+ssize_t rp1_ws281x_dma(const char *buf, ssize_t len);
+ssize_t rp1_ws281x_pwm_write(struct file *file, const char *buf, size_t total, loff_t *loff);
+int rp1_ws281x_pwm_probe(struct platform_device *pdev);
+void rp1_ws281x_pwm_remove(struct platform_device *pdev);
+
+
 #endif // __RP1_WS281X_PWM_H__
 
